15ab938a3d3392a47d30a3a9519ca9d03cbcca9f
Shinichi Hosomi <shinichi.hosomi@gmail.com>
2024-12-17 21:40:15 -0800

    add merge sort

--- C++/2_AddTwoNumbers.cpp
+++ C++/2_AddTwoNumbers.cpp
@@ -10,40 +10,58 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* pre = new ListNode();
-        ListNode* l3 = pre;
 
-        int co = 0;     // carry over
-        int val = 0;
+    int countNode(ListNode* l) {
+        int num = 0;
 
-        while (l1 != NULL || l2 != NULL || co != 0) {
-            val = co;
-            co = 0;
-            
-            if (l1 != NULL){
-                val += l1->val;
-                l1 = l1->next; 
-            } 
-            if (l2 != NULL){
-                val += l2->val;
-                l2 = l2->next;
-            } 
-
-            if (val > 9) {
-                val -= 10;
-                co = 1;
-            }
-
-            l3->next = new ListNode(val);
-            l3 = l3->next;
-            
+        while (l != nullptr) {
+            num++;
+            l = l->next;
         }
-
-        return pre->next;
-
+        return num;
     }
 
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int c = 0;  // carry over
+        int val = 0;
+
+        ListNode* head;
+        ListNode* main;
+
+        if (countNode(l1) < countNode(l2)) main = l2;
+        else main = l1;
+
+        head = main;
+
+        while(l1 != nullptr || l2 != nullptr) {
+            
+            val = c;
+            c = 0;
+
+            if (l1 != nullptr) val += l1->val;
+            if (l2 != nullptr) val += l2->val;
+            if (val > 9) {
+                val -= 10;
+                c = 1;
+            }
+
+            main->val = val;
+
+            if (l1 != nullptr) l1 = l1->next;
+            if (l2 != nullptr) l2 = l2->next;
+
+            if (l1 == nullptr && l2 == nullptr && c != 0) {
+                ListNode* newNode = new ListNode(c);
+                c = 0;
+                main->next = newNode;
+            }
+            main = main->next;
+        }
+
+        return head;
+    }
 };
 
-// algorithm linked list + memoty to carry over
+// 2,4,9
+// 5,6
+// 7,0,0,1
---
+++ TestSuite/C++/MergeSort.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <fstream>
+
+class MergeSort{
+public:
+    long long numOfInversion;
+
+    MergeSort():
+        numOfInversion(0)
+    {
+        ;    
+    }
+
+    std::vector<int> sep(std::vector<int>& arr, long long l, long long r){
+        if (l == r) {
+            std::vector<int> res(1,arr[l]);
+            return res;
+        }
+
+        long long m = (r + l) / 2;
+
+        std::vector<int> al = sep(arr, l, m);
+        std::vector<int> ar = sep(arr, m+1, r);
+
+        std::vector<int> alr(r-l+1,0);
+
+        //std::cout << l << "," << r << "," << alr.size() << std::endl;
+
+        long long idxl = 0;
+        long long idxr = 0;
+
+        for (size_t i=0;i<alr.size();i++){
+            if (idxl == al.size()) alr[i] = ar[idxr++];
+            else if (idxr == ar.size()) alr[i] = al[idxl++];
+            else if(al[idxl] > ar[idxr]) {
+                std::cout << al[idxl] << "," << ar[idxr] << ":" << al.size() << "#" << idxl << std::endl;
+                alr[i] = ar[idxr];
+                
+                //(this->numOfInversion)++;
+                //std::cout << ar.size() << "," << idxr << std::endl;
+                this->numOfInversion += al.size() - idxl;
+                idxr++;
+
+            }
+            else {
+                alr[i] = al[idxl];
+                idxl++;
+            }
+        }
+
+        return alr;
+    }
+
+    void cal(){
+        std::ifstream readFile("testdata.txt");
+        std::string text;
+
+        std::vector<int> arr;
+
+        while(getline (readFile, text)) {
+            std::cout << text << std::endl; 
+            arr.push_back(std::stoi(text));
+        }
+
+        std::vector<int> res = this->sep(arr,0,arr.size()-1);
+        std::cout << this->numOfInversion << std::endl;
+
+    }
+
+
+    void cal_test(){
+        std::vector<int> arr;
+        
+        // 3,2,1,4
+        
+        arr.push_back(3);
+        arr.push_back(2);
+        arr.push_back(1);
+        arr.push_back(4);
+        
+        std::vector<int> res = this->sep(arr,0,arr.size()-1);
+
+        std::cout << this->numOfInversion << std::endl;
+
+    }    
+};
+
+int main() {
+    MergeSort* mg = new MergeSort();
+    mg->cal();
+}
