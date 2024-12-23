625877cbfecaf867f25578d380e454a45c538d80
Shinichi Hosomi <shinichi.hosomi@gmail.com>
2024-12-22 21:18:15 -0800

    small change

--- C++/63_UniquePaths.cpp
+++ C++/63_UniquePaths.cpp
@@ -1,47 +1,30 @@
+using namespace std;
+#include <vector>
+
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int mR = obstacleGrid.size();
-        int mC = obstacleGrid[0].size();
-        long int map[mR][mC];
+        int rMax = obstacleGrid.size();
+        int cMax = obstacleGrid[0].size();
+        long long grid[rMax][cMax];
 
-        for (int r=0;r<mR;r++){
-            for (int c=0;c<mC;c++){
-                map[r][c]=0;
-            }
+        for (int r=0;r<rMax;r++){
+            for (int c=0;c<cMax;c++) grid[r][c] = 0;
         }
 
-        if (!obstacleGrid[mR-1][mC-1]) map[mR-1][mC-1] = 1;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[rMax-1][cMax-1] == 1) return 0;
 
-        for (int r=mR-1;r>-1;r--){
-            for (int c=mC-1;c>-1;c--){
-                if (!obstacleGrid[r][c]){
-                    if (r+1 < mR && !obstacleGrid[r+1][c]) {
-                        if (r+1 < mR) map[r][c]+=map[r+1][c];
-                    }
-                    if (c+1 < mC && !obstacleGrid[r][c+1]) {
-                        if (c+1 < mC) map[r][c]+=map[r][c+1];
-                    }
+        grid[rMax-1][cMax-1] = 1;
+
+        for(int r=rMax-1;r>-1;r--){
+            for(int c=cMax-1;c>-1;c--){
+                if (!obstacleGrid[r][c]) {
+                    if (r+1 < rMax && !obstacleGrid[r+1][c]) grid[r][c] += grid[r+1][c];
+                    if (c+1 < cMax && !obstacleGrid[r][c+1]) grid[r][c] += grid[r][c+1];
                 }
-            //cout << r << c << map[r][c] << endl;
             }
         }
-
-        return map[0][0];
+        
+        return grid[0][0];
     }
 };
-
-/*
-Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
-an m x n integer array grid
-move to the bottom-right corner
-An obstacle and space are marked 
-
-s 0 0
-0 1 0
-0 0 f
-
-path = sum of the right and down value.
-check whether the cell is obstable or not.
-
-*/
