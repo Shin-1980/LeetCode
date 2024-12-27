4c6ac03d6fe98ce48f01b5a59d75cbdf4d065e78
Shinichi Hosomi <shinichi.hosomi@gmail.com>
2024-12-27 15:33:37 -0800

    modified the case

--- C++/347_TopKFrequentElements.cpp
+++ C++/347_TopKFrequentElements.cpp
@@ -1,30 +1,32 @@
+using namespace std;
+#include <vector>
+#include <unordered_map>
+#include <priority_queue>
+
 class Solution {
 public:
-    static bool comp(pair<int, int> p1, pair<int, int> p2){
-
-        if (p1.second == p2.second) {
-            return p1.first > p2.first;
-        }
-
-        return p1.second > p2.second;
-    }
-
     vector<int> topKFrequent(vector<int>& nums, int k) {
 
-        unordered_map<int, int> mp;
-        for(auto num:nums) {
-            mp[num]++;
+        unordered_map<int, int> ht;
+
+        for (auto num:nums) {
+            ht[num] += 1;
         }
 
-        vector<pair<int, int> > freq_arr(mp.begin(), mp.end());
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minHeap;
 
-        sort(freq_arr.begin(), freq_arr.end(), comp);
-
-        vector<int> res;
-        for (int i=0;i<k;i++){
-            res.push_back(freq_arr[i].first);
+        for (auto& [element, frequency]: ht) {
+            minHeap.push({frequency, element});
+            if (minHeap.size() > k) minHeap.pop();
         }
 
-        return res;
+        vector<int> ans;
+
+        for (int i=0;i<k;i++) {
+            ans.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+
+        return ans;        
     }
 };
