d2355c230846132509884b644ce949199c23e3bb
Shinichi Hosomi <shinichi.hosomi@gmail.com>
2025-01-21 20:07:58 -0800

    updated script

--- Python3/347_TopKFrequentElements.py
+++ Python3/347_TopKFrequentElements.py
@@ -1,11 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        num_freq = Counter(nums)
+        ht = {}
+
+        for num in nums:
+            if num not in ht:
+                ht[num] = 1
+            else:
+                ht[num] += 1
+
         min_heap = []
 
-        for num, freg in num_freq.items():
-            heappush(min_heap, (freg, num))
+        for key, val in ht.items():
+            heappush(min_heap, (val, key))
 
             if len(min_heap) > k:
                 heappop(min_heap)
